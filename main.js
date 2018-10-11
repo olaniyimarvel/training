8771c8b62da28215d93b7e60422b425a945bc1fa
olaniyimarvel <olaniyimarvelous@gmail.com>
2018-10-10 21:44:34 -0700

    Group work

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ $('.header').height($(window).height());
+
+})
+/*header style*/
+body{
+ background-image: url('..mac2.jpg');
+ background-attachment: fixed;
+ background-size: cover;
+ background-position: center;
+}
